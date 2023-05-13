bcfc4eb69da6961ac35843e4b56d402bdf3396f5
Kadi-Liis Pärn <kadpar@ttu.ee>
2023-05-13 18:59:43 +0300

    nupp

--- app/src/components/Mainview.vue
+++ app/src/components/Mainview.vue
@@ -64,17 +64,14 @@
               </p>
             </li>
           </ul>
-          <button
-            @click.prevent="ToPage"
-            class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-400 hover:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            <span class="absolute left-0 inset-y-0 flex items-center pl-3">
-            </span>
-            Tracking Link
-          </button>
         </div>
       </div>
     </div>
+    <button
+            @click.prevent="ToPage"
+          >
+            {{ orders[0]?.shippingWebAddress }}
+          </button>
   </main>
 </template>
 
